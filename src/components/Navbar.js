941360124d5d2b7257da89e5901090867017b9ce
master-manas <manasmadhukar30@gmail.com>
2025-05-23 15:28:52 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,8 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+       <img src={Logo} alt="FlavorFiesta Logo" />
+
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
